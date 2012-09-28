231843040eb572ca348bd6c771cddbd9627f70db
Visgean Skeloru <Visgean@gmail.com>
2012-09-28 14:08:11 +0200

    media urls, baby!

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ MEDIA_ROOT = '/var/www/static/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/kvintang/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
