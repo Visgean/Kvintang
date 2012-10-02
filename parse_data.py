1dd7df9ab95ab0891b9476fb6783a4e8fc848022
Visgean Skeloru <Visgean@gmail.com>
2012-10-02 23:31:56 +0200

    data and data parser

---
+++ parse_data.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# @author: 	Visgean Skeloru 
+# email: 	<visgean@gmail.com>
+# jabber: 	<visgean@jabber.org>
+# github: 	http://github.com/Visgean
+
+
+import os, sys
+from kvintang.dict.models import Subject, Tag, Term
+
+data_files = os.listdir("data")
+
+subject = Subject.objects.get(name="Math")
+
+for filename in data_files:
+	with open("data/"+filename, "r") as ffile:
+		data = ffile.read()
+	
+	tag = Tag(name=filename)
+	tag.save()
+	
+	for line in data.splitlines():
+		trm = line.split("\t")
+		term = Term(englishVersion=trm[0], czechVersion=trm[1], subject=subject)
+		term.tags.add(tag)
+		term.save()
+		
+		
