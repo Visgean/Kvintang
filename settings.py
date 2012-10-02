70ce6d3495ab34087beca5fa86fa62ac48870b4f
Visgean Skeloru <Visgean@gmail.com>
2012-10-03 01:10:56 +0200

    passwrpds

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',	 # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': "kvintang",                      # Or path to database file if using sqlite3.
         'USER': 'kvintang',                      # Not used with sqlite3.
-        'PASSWORD': '3y4hTkWfXfgK',                  # Not used with sqlite3.
+        'PASSWORD': 'somepass',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
    	"charset": "utf8",
