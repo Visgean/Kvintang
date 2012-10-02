494f18969f54f9055e4727757e19cca3e9410123
Visgean Skeloru <Visgean@gmail.com>
2012-10-02 23:33:51 +0200

    ValueError: Term instance needs to have a primary key value before a many-to-many relationship can be used.

--- parse_data.py
+++ parse_data.py
@@ -24,6 +24,7 @@ for filename in data_files:
 	for line in data.splitlines():
 		trm = line.split("\t")
 		term = Term(englishVersion=trm[0], czechVersion=trm[1], subject=subject)
+		term.save()
 		term.tags.add(tag)
 		term.save()
 		
